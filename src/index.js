332af2d9f22ce6b70b685a9e3ae0deee5591fdf7
Anna Kulay <annakulay10@gmail.com>
2019-09-28 21:12:56 +0300

    feat add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const strLength = str.length;
+    const brackets = bracketsConfig.reduce((acc, item) => {
+            acc[item[0]] = item[1];
+            return acc;
+        }, {});
+    
+    let balancedBrackets = [];
+    for (let i = 0; i < strLength; i++) {
+        if (brackets[balancedBrackets[balancedBrackets.length - 1]] === str[i]) {
+            balancedBrackets.pop();
+        } else {
+            balancedBrackets.push(str[i]);
+        }
+    }
+    return !balancedBrackets.length;
 }
